122d7f67e876e40217186690ba72709939ce9931
peterducai <peter.ducai@telekom.com>
2018-05-21 09:41:28 +0200

    racks

---
+++ api_racks.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+//GetRacks get locations
+func GetRacks(c echo.Context) error {
+
+	racks, err := AllRacks()
+	if err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusBadRequest, "333")
+	}
+	for _, rm := range racks {
+		fmt.Println(rm.id, rm.rackName, rm.rackSize, rm.purchaseOrder, rm.rack)
+	}
+
+	return c.String(http.StatusOK, "racks!")
+}
---
+++ racks.go
@@ -0,0 +1,35 @@
+package main
+
+import "database/sql"
+
+//Rack in the data center
+type Rack struct {
+	id            sql.NullInt64
+	rackName      sql.NullString
+	rackSize      sql.NullInt64
+	purchaseOrder sql.NullInt64
+	rack          sql.NullString
+}
+
+//AllRacks will return Racks
+func AllRacks() ([]*Rack, error) {
+	rows, err := db.Query("SELECT * FROM aidb.Rack")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	Racks := make([]*Rack, 0)
+	for rows.Next() {
+		rk := new(Rack)
+		err := rows.Scan(&rk.id, &rk.rackName, &rk.rackSize, &rk.purchaseOrder, &rk.rack)
+		if err != nil {
+			return nil, err
+		}
+		Racks = append(Racks, rk)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return Racks, nil
+}
